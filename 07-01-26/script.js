5129ce754def2647305693194b9ce34400e27199
Abhijeet Ekka <abhijeetekka.dev@gmail.com>
2026-01-07 21:22:36 +0530

    add script to generate and log 10 random numbers

---
+++ 07-01-26/script.js
@@ -0,0 +1,14 @@
+let arr = [];
+
+//create a fun that push 10 random numbers
+
+function ran(n){
+   
+   for(let i = 0; i < n; i++){
+    let a = Math.floor(Math.random() * n);
+    arr.push(a);
+   }
+   console.log(arr);
+}
+
+ran(10);
