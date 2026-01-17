a59601143166d22554b14b9652793917c93b2d61
Abhijeet Ekka <abhijeetekka.dev@gmail.com>
2026-01-17 21:25:47 +0530

    add debugging HTML and JavaScript files with error examples

---
+++ January/17-01-26/script.js
@@ -0,0 +1,10 @@
+//Syntax Error
+
+// const arr = ["apple", "banana" "help"]; //comma missing this will give syntax Error
+
+//Refrence Error
+console.log(n);
+const n = 1;  //Uncaught ReferenceError: Cannot access 'n' before initialization
+
+
+//Type Error
