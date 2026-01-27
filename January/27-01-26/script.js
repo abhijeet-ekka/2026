8278f5af9a9564d7bf4fac7b1a598ae120453a4f
Abhijeet Ekka <abhijeetekka.dev@gmail.com>
2026-01-27 13:33:47 +0530

    update spam checker functionality and improve button styling

--- January/27-01-26/script.js
+++ January/27-01-26/script.js
@@ -7,11 +7,18 @@ function isSpam(msg){
     return plsRegex.test(msg)
 }
 
+console.log(isSpam(msgInput.value));
+
 console.log(isSpam("assist"))
 
 btn.addEventListener("click", () => {
    if(msgInput.value === ""){
     alert("Please enter a message.")
+    result.textContent = "";
+   }else if(isSpam(msgInput.value)){
+    result.textContent = "The message seems to be spam";
+   }else{
+    result.textContent = "This message doesn't seem to contain spam"
    }
-   return result.textContent = msgInput.value;
+   return  msgInput.value = "";
 })
