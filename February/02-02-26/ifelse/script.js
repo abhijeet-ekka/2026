3808138559b2160fd114854081ba7d5b57e67b46
Abhijeet Ekka <abhijeetekka.dev@gmail.com>
2026-02-03 16:39:31 +0530

    add initial HTML, CSS, and JavaScript files for password generator

---
+++ February/02-02-26/ifelse/script.js
@@ -0,0 +1,4 @@
+let num = 5 + 5;
+
+console.log(num == 10 ? `its ${num} ` : `its ${num} not 10`);
+
---
+++ February/03-02-26/pass-generator/script.js
@@ -0,0 +1,7 @@
+const passDisplay = document.getElementById("password-diplay");
+const btn = document.getElementById("button");
+
+
+function generator() {
+    let random = Math.ceil(Math.random()*)
+}
