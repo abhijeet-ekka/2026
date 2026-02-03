6da7bf99d47cd1e90fe1cf73db5103da88dd45ad
Abhijeet Ekka <abhijeetekka.dev@gmail.com>
2026-02-03 16:55:11 +0530

    fix: correct ID reference for password display and improve generator function

--- February/03-02-26/pass-generator/script.js
+++ February/03-02-26/pass-generator/script.js
@@ -1,7 +1,16 @@
-const passDisplay = document.getElementById("password-diplay");
+const passDisplay = document.getElementById("password-display");
 const btn = document.getElementById("button");
 
 
 function generator() {
-    let random = Math.ceil(Math.random()*)
+    let arr = [];
+
+    for(let i = 0; i < 10; i++){
+        let random = Math.ceil(Math.random() * 10);
+        arr.push(random)
+    }
+
+   passDisplay.textContent = arr.join("");
 }
+
+btn.addEventListener("click", generator);
